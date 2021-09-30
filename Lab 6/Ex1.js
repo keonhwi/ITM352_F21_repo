8be0bc98ae92748486d8e68975092524863e6a93
keonhwi <Keonhwi@hawaii.edu>
2021-09-30 02:39:33 -1000

    WOD update

--- Lab 6/Ex1.js
+++
@@ -1,8 +0,0 @@
-var age =  23;
-var fav_num = 0;
-var day_of_birth = 15;
-var month_of_birth = 4;
-
-console.log(age + fav_num / day_of_birth * month_of_birth);
-
-console.log( (age + fav_num) / day_of_birth * month_of_birth);
