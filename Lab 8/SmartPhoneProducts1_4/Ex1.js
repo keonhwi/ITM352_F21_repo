0fc2ee38f64b0fa54f685a5e431bc4af898e36a6
keonhwi <Keonhwi@hawaii.edu>
2021-10-12 23:35:23 -1000

    LAB 8

---
+++ Lab 8/SmartPhoneProducts1_4/Ex1.js
@@ -0,0 +1,8 @@
+require("./products_data.js");
+
+var num_products = 5;
+
+var count = 1;
+
+while(count <= num_products) {console.log(`${count}. ${eval('name'+count)}`);count++;}
+    console.log("That's all we have!")
