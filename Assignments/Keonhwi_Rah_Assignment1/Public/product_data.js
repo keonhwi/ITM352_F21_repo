7e2b00090b049707e58204516b24e19879e5675c
keonhwi <Keonhwi@hawaii.edu>
2021-11-18 20:07:13 -1000

    a

--- Assignments/Keonhwi_Rah_Assignment1/Public/product_data.js
+++ Assignments/Keonhwi_Rah_Assignment1/Public/product_data.js
@@ -3,25 +3,25 @@ var products = [
     
         {brand: "Pyzel",
         "price" : 799.00,
-        "image" : "./Images/surfboard1"},
+        "image" : "https://github.com/keonhwi/ITM352_F21_repo/blob/main/Assignments/Keonhwi_Rah_Assignment1/Public/Images/Surfboard1.jfif"},
 
     
         {brand : "Rob Machado",
         "price" : 499.00,
-        "image" :"./Images/surfboard2"},
+        "image" :"https://github.com/keonhwi/ITM352_F21_repo/blob/main/Assignments/Keonhwi_Rah_Assignment1/Public/Images/Surfboard2.jfif"},
 
     
         {brand : "Body Glove",
         "price" : 299.00,
-        "image" :"./Images/surfboard3"},
+        "image" :"https://github.com/keonhwi/ITM352_F21_repo/blob/main/Assignments/Keonhwi_Rah_Assignment1/Public/Images/Surfboard3.jfif"},
 
     
         {brand : "Giantex",
         "price" : 199.00,
-        "image" :"./Images/surfboard4"},
+        "image" :"https://github.com/keonhwi/ITM352_F21_repo/blob/main/Assignments/Keonhwi_Rah_Assignment1/Public/Images/Surfboard4.jfif"},
     
     
         {brand : "Flying Bee",
         "price" : 419.00,
-        "image" :"./Images/surfboard5"}
+        "image" :"https://github.com/keonhwi/ITM352_F21_repo/blob/main/Assignments/Keonhwi_Rah_Assignment1/Public/Images/Surfboard5.jfif"}
 ];
