dc36cd174315a923888e6d4284e7d25c91271066
keonhwi <Keonhwi@hawaii.edu>
2021-11-18 23:37:56 -1000

    idk

--- Assignments/Keonhwi_Rah_Assignment1/Public/product_data.js
+++ Assignments/Keonhwi_Rah_Assignment1/Public/product_data.js
@@ -3,25 +3,25 @@ var products = [
     
         {brand: "Pyzel",
         "price" : 799.00,
-        "image" : "https://github.com/keonhwi/ITM352_F21_repo/blob/main/Assignments/Keonhwi_Rah_Assignment1/Public/Images/Surfboard1.jfif"},
+        "image":"https://img.shaperbuddy.com/img/18/surfboards/5235215845eff7.jpg"},
 
     
         {brand : "Rob Machado",
         "price" : 499.00,
-        "image" :"https://github.com/keonhwi/ITM352_F21_repo/blob/main/Assignments/Keonhwi_Rah_Assignment1/Public/Images/Surfboard2.jfif"},
+        "image" :"http://cdn.shopify.com/s/files/1/1646/9407/products/GoFish_Red_1500px_1200x630.jpg?v=1627427041"},
 
     
         {brand : "Body Glove",
         "price" : 299.00,
-        "image" :"https://github.com/keonhwi/ITM352_F21_repo/blob/main/Assignments/Keonhwi_Rah_Assignment1/Public/Images/Surfboard3.jfif"},
+        "image" :"https://cdn.shopify.com/s/files/1/0103/0448/7482/products/stsurfdaily20u-343___daily-76-soft-top-surfboard-with-removable-fins-mint-white___default_2000x.jpg?v=1600730940"},
 
     
         {brand : "Giantex",
         "price" : 199.00,
-        "image" :"https://github.com/keonhwi/ITM352_F21_repo/blob/main/Assignments/Keonhwi_Rah_Assignment1/Public/Images/Surfboard4.jfif"},
+        "image" :"https://m.media-amazon.com/images/I/61PSvHFQvZL._AC_SL1200_.jpg"},
     
     
         {brand : "Flying Bee",
         "price" : 419.00,
-        "image" :"https://github.com/keonhwi/ITM352_F21_repo/blob/main/Assignments/Keonhwi_Rah_Assignment1/Public/Images/Surfboard5.jfif"}
+        "image" :"https://www.mundo-surf.com/43361/surfboard-ms-flying-bee-round.jpg"}
 ];
