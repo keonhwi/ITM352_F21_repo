5b5e917945d01375dd93db59b2b114fb31c12bd0
keonhwi <Keonhwi@hawaii.edu>
2021-11-18 19:58:29 -1000

    sample

---
+++ Assignments/Keonhwi_Rah_Assignment1/Public/product_data.js
@@ -0,0 +1,27 @@
+num_items= 5;
+var products = [
+    
+        {brand: "Pyzel",
+        "price" : 799.00,
+        "image" : "./Images/surfboard1"},
+
+    
+        {brand : "Rob Machado",
+        "price" : 499.00,
+        "image" :"./Images/surfboard2"},
+
+    
+        {brand : "Body Glove",
+        "price" : 299.00,
+        "image" :"./Images/surfboard3"},
+
+    
+        {brand : "Giantex",
+        "price" : 199.00,
+        "image" :"./Images/surfboard4"},
+    
+    
+        {brand : "Flying Bee",
+        "price" : 419.00,
+        "image" :"./Images/surfboard5"}
+];
